1e3ae5ba2d37419a41cb53a58e161539fe7495b4
Robin Genz <mail@robingenz.dev>
2020-12-04 23:51:46 +0100

    test(2020): add day_01 tests

--- 2020/day_01/code.py
+++ 2020/day_01/code.py
@@ -20,5 +20,11 @@ def main():
     print(f"Part 1: {part1(file_input)}")
     print(f"Part 2: {part2(file_input)}")
 
+def test():
+    test_input = [1721, 979, 366, 299, 675, 1456]
+    assert part1(test_input) == 514579
+    assert part2(test_input) == 241861950
+
 if __name__ == "__main__":
+    test()
     main()
