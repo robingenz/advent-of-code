7cad6165b4a213125412b74b9c27c08223200f42
Robin Genz <mail@robingenz.dev>
2020-12-04 23:50:32 +0100

    refactor: add type hints

--- 2020/day_01/code.py
+++ 2020/day_01/code.py
@@ -1,14 +1,14 @@
-def get_input():
+def get_input() -> list:
     with open("input.txt") as f:
         return [int(l) for l in f.readlines()]
 
-def part1(vals):
+def part1(vals: list) -> int:
     for idx_i, i in enumerate(vals):
         for j in vals[idx_i+1:]:
             if i + j == 2020:
                 return i * j
 
-def part2(vals):
+def part2(vals: list) -> int:
     for idx_i, i in enumerate(vals):
         for idx_j, j in enumerate(vals[idx_i+1:]):
             for o in vals[idx_j+1:]:
