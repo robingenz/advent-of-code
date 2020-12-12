6cd8c5b1c555f8c8957317abc212358907e5a131
Robin Genz <mail@robingenz.dev>
2020-12-12 23:01:35 +0100

    feat(2020): implement day 12

--- 2020/12/code.py
+++ 2020/12/code.py
@@ -1,6 +1,6 @@
 def get_input() -> list:
     with open(f"{__file__.rstrip('code.py')}input.txt") as f:
-        return [l[:-1] for l in f.readlines()]
+        return [l.strip() for l in f.readlines()]
 
 
 def format_input(lines: list) -> list:
@@ -8,11 +8,61 @@ def format_input(lines: list) -> list:
 
 
 def part1(vals: list) -> int:
-    return 0
+    x, y, orientation = 0, 0, 90
+    for instruction in vals:
+        action = instruction[:1]
+        value = int(instruction[1:])
+        if action == "N":
+            y += value
+        elif action == "S":
+            y -= value
+        elif action == "E":
+            x += value
+        elif action == "W":
+            x -= value
+        elif action == "L":
+            orientation = (360 + (orientation - value)) % 360
+        elif action == "R":
+            orientation = (orientation + value) % 360
+        elif action == "F":
+            if orientation == 0:
+                y += value
+            elif orientation == 180:
+                y -= value
+            elif orientation == 90:
+                x += value
+            elif orientation == 270:
+                x -= value
+    return abs(x) + abs(y)
 
 
 def part2(vals: list) -> int:
-    return 0
+    ship, waypoint = [0, 0], [10, 1]
+    for instruction in vals:
+        action = instruction[:1]
+        value = int(instruction[1:])
+        if action == "N":
+            waypoint[1] += value
+        elif action == "S":
+            waypoint[1] -= value
+        elif action == "E":
+            waypoint[0] += value
+        elif action == "W":
+            waypoint[0] -= value
+        elif action == "L" or action == "R":
+            for _ in range(0, value // 90):
+                if action == "L":
+                    tmp = waypoint[0]
+                    waypoint[0] = -waypoint[1]
+                    waypoint[1] = tmp
+                else:
+                    tmp = waypoint[0]
+                    waypoint[0] = waypoint[1]
+                    waypoint[1] = -tmp
+        elif action == "F":
+            ship[1] += waypoint[1] * value
+            ship[0] += waypoint[0] * value
+    return abs(ship[0]) + abs(ship[1])
 
 
 def main():
@@ -22,9 +72,15 @@ def main():
 
 
 def test():
-    test_input = format_input([])
-    assert part1(test_input) == 0
-    assert part2(test_input) == 0
+    test_input = format_input([
+        "F10",
+        "N3",
+        "F7",
+        "R90",
+        "F11"
+    ])
+    assert part1(test_input) == 25
+    assert part2(test_input) == 286
 
 
 if __name__ == "__main__":
