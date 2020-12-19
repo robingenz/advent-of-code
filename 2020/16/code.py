a08d69a799d6c7d2d1095863c1a28c36a2c0d4c7
Robin Genz <mail@robingenz.dev>
2020-12-19 21:37:31 +0100

    feat(2020): implement day 16 part 1

--- 2020/16/code.py
+++ 2020/16/code.py
@@ -3,15 +3,37 @@ def get_input() -> list:
         return [l.strip() for l in f.readlines()]
 
 
-def format_input(lines: list) -> list:
-    return lines
+def format_input(lines: list) -> dict:
+    obj = {
+        "ranges": [],
+        "tickets": []
+    }
+    for line in lines:
+        if "-" in line:
+            ranges = []
+            for val in line[line.find(":") + 2:].split(" or "):
+                ranges.append([int(num) for num in val.split("-")])
+            obj["ranges"].extend(ranges)
+        elif "," in line:
+            obj["tickets"].append([int(num) for num in line.split(",")])
+    return obj
 
 
-def part1(vals: list) -> int:
-    return 0
+def part1(obj: dict) -> int:
+    error_rate = 0
+    for ticket in obj["tickets"][1:]:
+        for val in ticket:
+            error = True
+            for r in obj["ranges"]:
+                if val >= r[0] and val <= r[1]:
+                    error = False
+                    break
+            if error:
+                error_rate += val
+    return error_rate
 
 
-def part2(vals: list) -> int:
+def part2(obj: dict) -> int:
     return 0
 
 
@@ -21,12 +43,5 @@ def main():
     print(f"Part 2: {part2(file_input)}")
 
 
-def test():
-    test_input = format_input([])
-    assert part1(test_input) == 0
-    assert part2(test_input) == 0
-
-
 if __name__ == "__main__":
-    test()
     main()
