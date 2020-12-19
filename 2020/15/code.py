976700f982f487415cd999967748b30ec0d5cf28
Robin Genz <mail@robingenz.dev>
2020-12-19 17:37:02 +0100

    feat(2020): implement day 15

--- 2020/15/code.py
+++ 2020/15/code.py
@@ -1,30 +1,38 @@
 def get_input() -> list:
     with open(f"{__file__.rstrip('code.py')}input.txt") as f:
-        return [l.strip() for l in f.readlines()]
+        return [int(num) for num in f.readline().split(",")]
 
 
-def format_input(lines: list) -> list:
-    return lines
+def run(vals: list, final: int) -> int:
+    start_at, last = len(vals) + 1, vals[-1]
+    mem = {val: [i + 1] for i, val in enumerate(vals)}
+    for i in range(start_at, final + 1):
+        last = 0 if len(mem[last]) == 1 else mem[last][-1] - mem[last][-2]
+        if last in mem:
+            mem[last].append(i)
+        else:
+            mem[last] = [i]
+    return last
 
 
 def part1(vals: list) -> int:
-    return 0
+    return run(vals, 2020)
 
 
 def part2(vals: list) -> int:
-    return 0
+    return run(vals, 30000000)
 
 
 def main():
-    file_input = format_input(get_input())
+    file_input = get_input()
     print(f"Part 1: {part1(file_input)}")
     print(f"Part 2: {part2(file_input)}")
 
 
 def test():
-    test_input = format_input([])
-    assert part1(test_input) == 0
-    assert part2(test_input) == 0
+    test_input = [0, 3, 6]
+    assert part1(test_input) == 436
+    assert part2(test_input) == 175594
 
 
 if __name__ == "__main__":
