cdf6544bba253b2b9512852ca1fa38169cd6c587
Robin Genz <mail@robingenz.dev>
2020-12-22 22:49:18 +0100

    part 1

--- 2020/22/code.py
+++ 2020/22/code.py
@@ -4,14 +4,38 @@ def get_input() -> list:
 
 
 def parse_input(lines: list) -> list:
-    return lines
+    decks = list()
+    for line in lines:
+        if "Player" in line:
+            decks.append(list())
+        elif line.isnumeric():
+            decks[-1].append(int(line))
+    print(decks)
+    return decks
 
 
-def part1(vals: list) -> int:
-    return 0
+def part1(decks: list) -> int:
+    while len(decks[0]) > 0 and len(decks[1]) > 0:
+        print(decks[0][0], decks[1][0])
+        if decks[0][0] > decks[1][0]:
+            decks[0].append(decks[0][0])
+            decks[0].append(decks[1][0])
+            decks[0].pop(0)
+            decks[1].pop(0)
+        else:
+            decks[1].append(decks[1][0])
+            decks[1].append(decks[0][0])
+            decks[0].pop(0)
+            decks[1].pop(0)
+    winner = 0 if len(decks[0]) > 0 else 1
+    decks[winner].reverse()
+    score = 0
+    for i in range(len(decks[winner])):
+        score += decks[winner][i] * (i + 1)
+    return score
 
 
-def part2(vals: list) -> int:
+def part2(decks: list) -> int:
     return 0
 
 
@@ -21,12 +45,5 @@ def main():
     print(f"Part 2: {part2(file_input)}")
 
 
-def test():
-    test_input = parse_input([])
-    assert part1(test_input) == 0
-    assert part2(test_input) == 0
-
-
 if __name__ == "__main__":
-    test()
     main()
