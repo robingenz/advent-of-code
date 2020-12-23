206c1be597c4bdb4b33e42f627a2e84415e8c12a
Robin Genz <mail@robingenz.dev>
2020-12-23 21:23:34 +0100

    feat(2020): implement day 23 (#4)
    * part 1
    
    * part 2

--- 2020/23/code.py
+++ 2020/23/code.py
@@ -1,18 +1,60 @@
 def get_input() -> list:
     with open(f"{__file__.rstrip('code.py')}input.txt") as f:
-        return [l.strip() for l in f.readlines()]
+        return f.read().strip()
 
 
-def parse_input(lines: list) -> list:
-    return lines
+def parse_input(line: str) -> list:
+    return list(map(int, line))
 
 
-def part1(vals: list) -> int:
-    return 0
+class Cup:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
 
 
-def part2(vals: list) -> int:
-    return 0
+def play(cups: list, moves: int) -> Cup:
+    size, cups_dict = len(cups), dict()
+    for i in range(size):
+        cups[i].next = cups[(i + 1) % size]
+        cups_dict[cups[i].value] = cups[i]
+    maximum, minimum = max(cups_dict.keys()), min(cups_dict.keys())
+    current_cup = cups[0]
+    for _ in range(moves):
+        picked_up_cup = current_cup.next
+        current_cup.next = current_cup.next.next.next.next
+        destination_cup_value = current_cup.value
+        forbidden_values = [current_cup.value,
+                            picked_up_cup.value,
+                            picked_up_cup.next.value,
+                            picked_up_cup.next.next.value]
+        while destination_cup_value in forbidden_values:
+            destination_cup_value -= 1
+            if destination_cup_value < minimum:
+                destination_cup_value = maximum
+        destination_cup = cups_dict[destination_cup_value]
+        picked_up_cup.next.next.next = destination_cup.next
+        destination_cup.next = picked_up_cup
+        current_cup = current_cup.next
+    return cups_dict[1]
+
+
+def part1(val: int) -> int:
+    cups, cups_size = [Cup(n) for n in val], len(val)
+    cup_1 = play(cups, 100)
+    result, next_cup = "", cup_1.next
+    for _ in range(cups_size - 1):
+        result += str(next_cup.value)
+        next_cup = next_cup.next
+    return int(result)
+
+
+def part2(val: int) -> int:
+    cups = [Cup(n) for n in range(1, 1000000 + 1)]
+    for k, v in enumerate(val):
+        cups[k] = Cup(v)
+    cup_1 = play(cups, 10000000)
+    return cup_1.next.value * cup_1.next.next.value
 
 
 def main():
@@ -22,9 +64,9 @@ def main():
 
 
 def test():
-    test_input = parse_input([])
-    assert part1(test_input) == 0
-    assert part2(test_input) == 0
+    test_input = parse_input("389125467")
+    assert part1(test_input) == 67384529
+    assert part2(test_input) == 149245887792
 
 
 if __name__ == "__main__":
