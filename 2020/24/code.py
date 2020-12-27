895787a72791e5ad2d66f04323ad82167221ed4f
Robin Genz <mail@robingenz.dev>
2020-12-27 13:15:25 +0100

    part 1

--- 2020/24/code.py
+++ 2020/24/code.py
@@ -1,17 +1,44 @@
+
 def get_input() -> list:
     with open(f"{__file__.rstrip('code.py')}input.txt") as f:
         return [l.strip() for l in f.readlines()]
 
 
 def parse_input(lines: list) -> list:
-    return lines
+    instructions = list()
+    for line in lines:
+        i = 0
+        instructions.append([])
+        while i < len(line):
+            if line[i] == "s" or line[i] == "n":
+                instructions[-1].append(line[i:i+2])
+                i += 2
+                continue
+            instructions[-1].append(line[i])
+            i += 1
+    return instructions
 
 
-def part1(vals: list) -> int:
-    return 0
+def run(instructions: list) -> dict:
+    dirs = {"e": (2, 0), "se": (1, -1), "sw": (-1, -1),
+            "w": (-2, 0), "nw": (-1, 1), "ne": (1, 1)}
+    result = {
+        (0, 0): True
+    }
+    for instruction in instructions:
+        coords = (0, 0)
+        for item in instruction:
+            coords = (coords[0] + dirs[item][0], coords[1] + dirs[item][1])
+        result[coords] = not result[coords] if coords in result else False
+    return result
 
 
-def part2(vals: list) -> int:
+def part1(instructions: list) -> int:
+    tiles = run(instructions)
+    return len([v for _, v in tiles.items() if v == False])
+
+
+def part2(instructions: list) -> int:
     return 0
 
 
@@ -21,12 +48,5 @@ def main():
     print(f"Part 2: {part2(file_input)}")
 
 
-def test():
-    test_input = parse_input([])
-    assert part1(test_input) == 0
-    assert part2(test_input) == 0
-
-
 if __name__ == "__main__":
-    test()
     main()
