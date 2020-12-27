5346dbf1be2b6fda870245fd4caa50816e526bdb
Robin Genz <mail@robingenz.dev>
2020-12-27 15:03:31 +0100

    part 2 wip

--- 2020/24/code.py
+++ 2020/24/code.py
@@ -19,27 +19,52 @@ def parse_input(lines: list) -> list:
     return instructions
 
 
-def run(instructions: list) -> dict:
-    dirs = {"e": (2, 0), "se": (1, -1), "sw": (-1, -1),
+def get_directions() -> dict:
+    return {"e": (2, 0), "se": (1, -1), "sw": (-1, -1),
             "w": (-2, 0), "nw": (-1, 1), "ne": (1, 1)}
-    result = {
+
+
+def run(instructions: list) -> dict:
+    dirs = get_directions()
+    tiles = {
         (0, 0): True
     }
     for instruction in instructions:
         coords = (0, 0)
         for item in instruction:
             coords = (coords[0] + dirs[item][0], coords[1] + dirs[item][1])
-        result[coords] = not result[coords] if coords in result else False
-    return result
+        tiles[coords] = not tiles[coords] if coords in tiles else False
+    return tiles
+
+
+def get_neighbours(tile: tuple, tiles: dict) -> dict:
+    neighbours, dirs = dict(), get_directions()
+    for v in dirs.values():
+        coord = (tile[0] + v[0], tile[1] + v[1])
+        neighbours[coord] = tiles[coord] if coord in tiles else True
+    return neighbours
 
 
 def part1(instructions: list) -> int:
     tiles = run(instructions)
-    return len([v for _, v in tiles.items() if v == False])
+    return len([v for v in tiles.values() if v == False])
 
 
 def part2(instructions: list) -> int:
-    return 0
+    tiles = run(instructions)
+    for _ in range(100):
+        updates = dict()
+        for k, v in tiles.items():
+            neighbours = get_neighbours(k, tiles)
+            updates.update(neighbours)
+            black_neighbours_size = len(
+                [n for n in neighbours.values() if n == False])
+            if v == False and (black_neighbours_size == 0 or black_neighbours_size > 2):
+                updates[k] = True
+            elif v == True and black_neighbours_size == 2:
+                updates[k] = False
+        tiles.update(updates)
+    return len([v for v in tiles.values() if v == False])
 
 
 def main():
